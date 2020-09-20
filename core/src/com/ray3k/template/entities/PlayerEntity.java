465e8b2dc8112b2801b16196ea44a10ab6e86a8b
raeleus <raymond.ray3k@gmail.com>
2020-09-19 22:27:39 -0700

    Added basic movement to player.

--- core/src/com/ray3k/template/entities/PlayerEntity.java
+++ core/src/com/ray3k/template/entities/PlayerEntity.java
@@ -1,8 +1,13 @@
 package com.ray3k.template.entities;
 
+import com.ray3k.template.*;
+
+import static com.ray3k.template.Core.Binding.*;
 import static com.ray3k.template.Resources.*;
+import static com.ray3k.template.screens.GameScreen.*;
 
 public class PlayerEntity extends Entity {
+    public static final float MOVE_SPEED = 50f;
     @Override
     public void create() {
         setSkeletonData(spine_player, spine_playerAnimationData);
@@ -15,7 +20,21 @@ public class PlayerEntity extends Entity {
     
     @Override
     public void act(float delta) {
-    
+        if (gameScreen.isAnyBindingPressed(LEFT, RIGHT, UP, DOWN)) {
+            setSpeed(MOVE_SPEED);
+            float direction = 0;
+            if (gameScreen.isAnyBindingPressed(LEFT, RIGHT)) {
+                if (gameScreen.isBindingPressed(LEFT)) direction = 180;
+                
+                if (gameScreen.isBindingPressed(UP)) direction = Utils.approach360(direction, 90, 45);
+                else if (gameScreen.isBindingPressed(DOWN)) direction = Utils.approach360(direction, 270, 45);
+            } else if (gameScreen.isBindingPressed(UP)) direction = 90;
+            else if (gameScreen.isBindingPressed(DOWN)) direction = 270;
+            
+            setDirection(direction);
+        } else {
+            setSpeed(0);
+        }
     }
     
     @Override
